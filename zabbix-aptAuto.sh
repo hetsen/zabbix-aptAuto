c64113b39dbcd949a59aaa2e3be11fd92dca4ae9
hetsen <creftan@gmail.com>
2021-11-09 10:30:21 +0100

    Update zabbix-aptAuto.sh

--- zabbix-aptAuto.sh
+++ zabbix-aptAuto.sh
@@ -4,8 +4,15 @@ echo 'Cloning zabbix-apt'
 git clone https://github.com/theranger/zabbix-apt
 echo 'Copying 02peridic to /etc/apt/apt.conf.d/'
 cp zabbix-apt/apt.conf.d/02periodic /etc/apt/apt.conf.d/
-echo 'Copying apt.conf to /etc/zabbix/zabbix_agentd.d/'
-cp zabbix-apt/zabbix_agentd.d/apt.conf /etc/zabbix/zabbix_agentd.d/
+echo 'Copying apt.conf to /etc/zabbix/zabbix_agentd.d/ or /etc/zabbix/zabbix_agentd.conf.d/'
+
+if [ -d "/etc/zabbix/zabbix_agentd.d" ]; then
+  echo '/etc/zabbix/zabbix_agentd.d/'
+  cp zabbix-apt/zabbix_agentd.d/apt.conf /etc/zabbix/zabbix_agentd.d/
+else
+  echo '/etc/zabbix/zabbix_agentd.conf.d/ it is'
+  cp zabbix-apt/zabbix_agentd.d/apt.conf /etc/zabbix/zabbix_agentd.conf.d/
+fi
 echo 'Adding Timeout=30 to /etc/zabbix/zabbix/agentd.conf'
 sed -i 's/# Timeout=3/Timeout=30/' /etc/zabbix/zabbix_agentd.conf
 echo 'Restarting zabbix-agent'
